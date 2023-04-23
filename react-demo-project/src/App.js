89779f8854ec5e4acffb3fc93529a6b8c9adca61
a6hi <saraffabhishek@outlook.com>
2023-04-23 00:53:38 -0400

    functions as props (with bind) WORKING

--- react-demo-project/src/App.js
+++ react-demo-project/src/App.js
@@ -4,14 +4,16 @@ import MyComponent from "./MyComponent";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.thisComponentsLog = this.consoleLogFunction.bind(this);
   }
 
   consoleLogFunction() {
+    console.log(this);
     console.log("button has been clicked");
   }
 
   render() {
-    return <MyComponent title="React" action={this.consoleLogFunction} />;
+    return <MyComponent title="React" action={this.thisComponentsLog} />;
   }
 }
 
