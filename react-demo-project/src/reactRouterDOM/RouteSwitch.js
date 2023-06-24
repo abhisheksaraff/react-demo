66cedb1202491dd5cfb5dddcd863f816066ea332
a6hi <saraffabhishek@outlook.com>
2023-06-23 23:04:08 -0400

    added navbar to react-router-dom

--- react-demo-project/src/reactRouterDOM/RouteSwitch.js
+++ react-demo-project/src/reactRouterDOM/RouteSwitch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; //npm i react-router-dom
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //npm i react-router-dom
 import App from "./App";
 import Profile from "./Profile";
 
@@ -10,6 +10,12 @@ const RouteSwitch = () => {
         <Route path="/" element={<App />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
+
+      <div className="navbar">
+        <Link to="/">App</Link>
+        <br/>
+        <Link to="/profile">Profile</Link>
+      </div>
     </BrowserRouter>
   );
 };
